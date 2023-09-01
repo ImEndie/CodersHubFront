60d1ad82cb5f366f415c81df2c0ecce8f744acf4
Endie <a-abdusattorov@outlook.com>
2023-09-02 01:19:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="sidebar">
+        <h1>CodersHub</h1>
+        <div>
+            <input
+              id="q"
+              aria-label="Search CodersHub"
+              placeholder="Search CodersHub"
+              type="search"
+              name="q"
+              style={{width: "70%"}}
+            />
+            <button onClick={()=>window.open('/search/'+document.getElementById('q').value,'_self')}>Search</button>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href={`/`}>Home</a>
+            </li>
+            <li>
+              <a href={`/blogs`}>Blogs</a>
+            </li>
+            <li>
+              <a href={`/create`}>Create Post</a>
+            </li>
+            <li>
+              <a href={`/profile`}>Profile</a>
+            </li>
+            <li>
+              <a href={`/logout`} style={{color: "red"}}>Log out</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div id="childrenElement"><Outlet/></div>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,11 +3,79 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './error-page';
+import Home from './routes/home';
+import ProfilePage from './routes/profile';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import BlogsPage from './routes/blogs';
+import LoginPage from './routes/login';
+import RegisterPage from './routes/reg';
+import CreatePost from './routes/createPost';
+import Blog from './routes/blog';
+import Search from './routes/search';
+
+library.add(fab, fas)
+
+function Logout(){
+  sessionStorage.clear();window.open('/login','_self');return (<></>)
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    ErrorBoundary: ErrorPage,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "profile",
+        element: <ProfilePage/>
+      },
+      {
+        path: "blogs",
+        element: <BlogsPage/>
+      },
+      {
+        path: "login",
+        element: <LoginPage/>
+      },
+      {
+        path: 'register',
+        element: <RegisterPage/>
+      },
+      {
+        path: 'logout',
+        element: <Logout/>
+      },
+      {
+        path: 'create',
+        element: <CreatePost />
+      },
+      {
+        path: 'blogs/:blogId',
+        element: <Blog />
+      },
+      {
+        path: 'search/:keyword',
+        element: <Search />
+      }
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
